a53e74a32608f0ee911d6d44b6618e8d6c6be4fa
IrinaKdr <irina.kdr@gmail.com>
2020-02-09 19:59:45 +0200

    Improve chat formatting

--- UI/Chat.gd
+++ UI/Chat.gd
@@ -21,11 +21,11 @@ func _input(event: InputEvent) -> void:
 
 
 func announce_connected(id: int) -> void:
-	$Panel/ChatWindow.bbcode_text += "\n[color=gray]%d has joined the game.[/color]" % id
+	$Panel/ChatWindow.bbcode_text += "\n%s [color=yellow]%d has joined the game.[/color]" % [_time_string(), id]
 
 
 func announce_disconnected(id: int) -> void:
-	$Panel/ChatWindow.bbcode_text += "\n[color=gray]%d has left the game.[/color]" % id
+	$Panel/ChatWindow.bbcode_text += "\n%s [color=yellow]%d has left the game.[/color]" % [_time_string(), id]
 
 
 func send_message(message: String) -> void:
@@ -34,4 +34,9 @@ func send_message(message: String) -> void:
 
 
 sync func print_message(id: int, message: String) -> void:
-	$Panel/ChatWindow.bbcode_text += "\n[color=green]%d[/color]: %s" % [id, message]
+	$Panel/ChatWindow.bbcode_text += "\n%s [[color=green]%d[/color]]: %s" % [_time_string(), id, message]
+
+
+func _time_string() -> String:
+	var time: Dictionary = OS.get_time()
+	return "[color=gray][%d:%d:%d][/color]" % [time.hour, time.minute, time.second]
