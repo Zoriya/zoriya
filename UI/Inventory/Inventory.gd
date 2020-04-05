dd73312904be24782b26fe3e86245f982d8f0565
Shatur95 <genaloner@gmail.com>
2020-04-05 14:40:45 +0300

    Control mouse on inventory toggling

--- UI/Inventory/Inventory.gd
+++ UI/Inventory/Inventory.gd
@@ -14,7 +14,12 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("inventory"):
-		visible = !visible
+		if visible:
+			visible = false
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			visible = true
 
 
 func pick_item(item: Item) -> void:
